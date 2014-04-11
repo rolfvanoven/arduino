6574f9410a64d8e739ca36c78d95598ff85fe341
Rolf van Oven <rolfvanoven@users.noreply.github.com>
2014-04-12 00:32:32 +0200

    Update neopixel_swirl.ino

--- neopixel_swirl.ino
+++ neopixel_swirl.ino
@@ -11,25 +11,48 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, 6, NEO_GRB + NEO_KHZ800);
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+/*
+// brightness hier (1-255)
+int brightness = 255;
 
-int rood = 100;
+// snelheid hier (1-25?:
+int snelheid = 5;
+
+//draairichting (1 = tegen de klok, -1 = met de klok);
+int draairichting = 1;
+*/
+
+
+/*
+int rood = 0;
 int groen = 0;
-int blauw=100;
-int k = 0;
+int blauw=0;
+*/
 
+
+int k = 0;
+//int h = brightness;
+int s = 40;
+int r = 0;
+int g = 0;
+int b = 0;
+//int d = draairichting;
 
 void setup() {
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show(); // Initialize all pixels to 'off
+  
+  //voor debugging
+//  Serial.begin(9600);
 }
 
 
 
 
 void loop() {
-  for (int p=0; p<16; p++) {
+  for (int p = 15; p > -1; p--) {
       
-      if(k<300) {
+      if(k < 768) {
        k = k + 1;
       } else {
         k = 0;
@@ -39,45 +62,59 @@ void loop() {
         
         
         
-      if(k < 100) {
-      rood = 100 - k;
-      groen = k;
-      blauw = 0;
-    } else if(k < 200) {
-      rood = 0;
-      groen = 200 - k;
-      blauw = k -100;
+      if(k < 256) {
+      r = 256 - k;
+      g = k;
+      b = 0;
+    } else if(k < 512) {
+      r = 0;
+      g = 512 - k;
+      b = k -256;
     } else {
-      rood = k - 200;
-      groen = 0;
-      blauw = 300 - k;
+      r = k - 512;
+      g = 0;
+      b = 768 - k;
     }
-
-
-   
-   
-   
-        
+    
+ 
       
-      strip.setPixelColor(p, strip.Color(rood, groen, blauw));
-      strip.setPixelColor(p-1, strip.Color(rood * 0.8, groen * 0.8, blauw*0.8));
-      strip.setPixelColor(p-2, strip.Color(rood * 0.6, groen * 0.6, blauw*0.6));
-      strip.setPixelColor(p-3, strip.Color(rood * 0.4, groen * 0.4, blauw*0.4));
-      strip.setPixelColor(p-4, strip.Color(rood * 0.2, groen * 0.2, blauw*0.2));
-      strip.setPixelColor(p-5, 0);
-      strip.setPixelColor(p+15, strip.Color(rood * 0.8, groen * 0.8, blauw*0.8));
-      strip.setPixelColor(p+14, strip.Color(rood * 0.6, groen * 0.6, blauw*0.6));
-      strip.setPixelColor(p+13, strip.Color(rood * 0.4, groen * 0.4, blauw*0.4));
-      strip.setPixelColor(p+12, strip.Color(rood * 0.2, groen * 0.2, blauw*0.2));
-      strip.setPixelColor(p+11, 0);
+      strip.setPixelColor(p, strip.Color(r, g, b));
+      strip.setPixelColor(p+1, strip.Color(r * 0.9, g * 0.9, b * 0.9));
+      strip.setPixelColor(p+2, strip.Color(r * 0.8, g * 0.8, b * 0.8));
+      strip.setPixelColor(p+3, strip.Color(r * 0.7, g * 0.7, b * 0.7));
+      strip.setPixelColor(p+4, strip.Color(r * 0.6, g * 0.6, b * 0.6));
+      strip.setPixelColor(p+5, strip.Color(r * 0.5, g * 0.5, b * 0.5));
+      strip.setPixelColor(p+6, strip.Color(r * 0.4, g * 0.4, b * 0.4));
+      strip.setPixelColor(p+7, strip.Color(r * 0.3, g * 0.3, b * 0.3));
+      strip.setPixelColor(p+8, strip.Color(r * 0.2, g * 0.2, b * 0.2));
+      strip.setPixelColor(p+9, strip.Color(r * 0.1, g * 0.1, b * 0.1));
+      strip.setPixelColor(p+10, 0);
 
+      strip.setPixelColor(p-15, strip.Color(r * 0.9, g * 0.9, b * 0.9));
+      strip.setPixelColor(p-14, strip.Color(r * 0.8, g * 0.8, b * 0.8));
+      strip.setPixelColor(p-13, strip.Color(r * 0.7, g * 0.7, b * 0.7));
+      strip.setPixelColor(p-12, strip.Color(r * 0.6, g * 0.6, b * 0.6));
+      strip.setPixelColor(p-11, strip.Color(r * 0.5, g * 0.5, b * 0.5));
+      strip.setPixelColor(p-10, strip.Color(r * 0.4, g * 0.4, b * 0.4));
+      strip.setPixelColor(p-9, strip.Color(r * 0.3, g * 0.3, b * 0.3));
+      strip.setPixelColor(p-8, strip.Color(r * 0.2, g * 0.2, b * 0.2));
+      strip.setPixelColor(p-7, strip.Color(r * 0.1, g * 0.1, b * 0.1));
+      strip.setPixelColor(p-6, 0);
 
 
 
     strip.show();
-    delay(25);
-    
-    
+    delay(s);
+
+
+/*
+    Serial.println(p);
+    Serial.println(k);
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
+*/
+   
     
     }
 
